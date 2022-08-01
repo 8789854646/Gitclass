4fdd6ae778f4f7531fe2f56bbd5d56acd408eba1
8789854646 <vivekkumarkushwaha58@gmail.com>
2022-08-01 22:13:36 +0530

    Added one file

--- practice.py
+++ practice.py
@@ -1,25 +1,16 @@
-class Parent:
-	def __init__(self, a, b):
-		self.a = a
-		self.b = b
-	def add(self):
+class A:
+	def demo(self):
 		print("Print Parent class")
-		return self.a + self.b
-	def sub(self):
-		self.add()
 
-class Child(Parent):
-	def __init__(self, a, b):
-		self.a = a
-		self.b = b
-	def mul(self):
+class B(A):
+	def demo(self):
 		print("Print Child Class")
-		return self.a * self.b
 
-p = Parent(25, 36)
+class C(B):
+	def demo(self):
+		print("Print C Class")
 
-vivek
-
+c = C()
 
 
 
