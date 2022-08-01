3d2fb250a7474623adc0b476c45014062706425f
8789854646 <vivekkumarkushwaha58@gmail.com>
2022-08-01 11:19:42 +0530

    adding practice.py

---
+++ practice.py
@@ -0,0 +1,20 @@
+class Parent:
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+	def add(self):
+		print("Print Parent class")
+		return self.a + self.b
+	def sub(self):
+		self.add()
+
+class Child(Parent):
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+	def mul(self):
+		print("Print Child Class")
+		return self.a * self.b
+
+p = Parent(25, 36)
+
